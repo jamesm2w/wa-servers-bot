d506aa3c8d741bfd2c8775ef49c4793f1ec55c8e
Glitch (wa-server-bot) <none>
2018-05-27 15:17:19 +0000

    🚧⏲ Checkpoint ./server.js:329397/1180

--- server.js
+++ server.js
@@ -1,8 +1,54 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const http = require("http");
+const https = require("https");
+
+var getJSON = function(options, onResult) {
+    console.log("getjson called");
+    var prot = options.port == 443 ? https : http;
+    var req = prot.request(options, function(res)
+    {
+        var output = '';
+        console.log(options.host + ':' + res.statusCode);
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            output += chunk;
+        });
+        res.on('end', function() {
+            var obj = JSON.parse(output);
+            onResult(res.statusCode, obj);
+        });
+    });
+    req.on('error', function(err) {
+        //res.send('error: ' + err.message);
+        throw err;
+    });
+    req.end();
+};
+
+var waoptions = {
+    host: 'worldsadrift.api.bossagames.com',
+    port: 80,
+    path: "/deploymentStatus",
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+};
+
+var latestData = {}
+
+setTimeout(()=>{
+  getJSON(waoptions, (status, result) => {
+    if (status == 200) {
+      latestData = result;
+    }
+  });
+}, 56000);
 
 client.on('ready', () => {
   console.log("Logged in");
+  client.setPresence();
 });
 
 client.on('message', msg => {
