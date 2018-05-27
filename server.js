9d3833ef5b46b3bf22529732df07962d2fdb03e3
Glitch (wa-server-bot) <none>
2018-05-27 14:53:42 +0000

    🐡💠 Checkpoint ./server.js:329397/930 ./package.json:329397/1227

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
-// server.js
-// where your node app starts
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-// init project
-var express = require('express');
-var app = express();
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
+
+client.login('token');
