a57be5be7f44dd7c4043c62a096a4222d9e5a0e5
Glitch (wa-server-bot) <none>
2018-05-28 14:04:07 +0000

    😳🍳 Checkpoint ./server.js:329397/2149 ./.glitch-assets:329397/1748

--- server.js
+++ server.js
@@ -1,84 +1,27 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
 const http = require("http");
 const https = require("https");
 
-var getJSON = function(options, onResult) {
-    console.log("getjson called");
-    var prot = options.port == 443 ? https : http;
-    var req = prot.request(options, function(res)
-    {
-        var output = '';
-        console.log(options.host + ':' + res.statusCode);
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            output += chunk;
-        });
-        res.on('end', function() {
-            var obj = JSON.parse(output);
-            onResult(res.statusCode, obj);
-        });
-    });
-    req.on('error', function(err) {
-        //res.send('error: ' + err.message);
-        throw err;
-    });
-    req.end();
-};
+const capulca_client = new Discord.Client();
+const tenrui_client = new Discord.Client();
 
-var waoptions = {
-    host: 'worldsadrift.api.bossagames.com',
-    port: 80,
-    path: "/deploymentStatus",
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-};
+function getServerStatus() {
 
-var latestData = {}, latestDiscordStatus = [];
+}
 
-setTimeout(()=>{
-  getJSON(waoptions, (status, result) => {
-    if (status == 200) {
-      latestData = result;
-    }
-    for (var i = 0; i < servers.length; i++){
-      latestDiscordStatus[i] = getPresence(latestData[servers[i]]);
-    }
-  });
-}, 56000);
-
-var servers = Object.keys(latestData);
-
-var getPresence = (server) => {
-  let serverStatus = latestData[server], discordStatus;
-  switch (serverStatus.status) {
-    case "up":
-      discordStatus = "online";
-    case "maintenance":
-      discordStatus = "idle";
-    case "down":
-      discordStatus = "dnd";
-  }
-  return {"game": {"name": "on " + serverStatus.name + " | " + serverStatus.status + " | " + serverStatus.population, "type": "PLAYING"}, "status": discordStatus}
-};
-
-client.on('ready', () => {
-  console.log("Logged in");
-  let i = 0;
-  setTimeout(() => {
-    client.user.setPresence(latestDiscordStatus[i]);
-    i++;
-  }, 28000);
+tenrui_client.on("ready", () => {
+  console.log("Ten-Rui Logged in");
 });
 
+capulca_client.on('ready', () => {
+  console.log("Capulca Logged in");
+});
 
-
-client.on('message', msg => {
+capulca_client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
 });
 
-client.login(process.env.TOKEN);
+capulca_client.login(process.env.CAPULCA_TOKEN);
+tenrui_client.login(process.env.TENRUI_TOKEN);
