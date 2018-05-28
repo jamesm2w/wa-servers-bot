c0733f48152088fc5c52e16367fd00b7e7b5d59e
Glitch (wa-server-bot) <none>
2018-05-28 15:18:55 +0000

    🌒🏅 Checkpoint ./server.js:329397/1551

--- server.js
+++ server.js
@@ -9,11 +9,26 @@ const clients = {
   "us_03": new Discord.Client()
 }
 
+function toTitleCase(str){
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
 var latestStatus = {};
 
 function updateBots() {
   for (let [serverID, client] of Object.entries(clients)) {
-    client.user.setPresence({"game": "", {}})
+    let status, avatarURL, report;
+    if (latestStatus[serverID].status == "up") {
+      client.user.setPresence({"game": {"name": "Online | " + toTitleCase(latestStatus[serverID].population) + " Population"}, "status": "online"})
+      
+    } else if (latestStatus[serverID].status == "maintenance") {
+      client.user.setPresence({"game": {"name": "Maintenance | " + toTitleCase(latestStatus[serverID].population) + " Population"}, "status": "idle"})
+      
+    } else if (latestStatus[serverID].status == "down") {
+      client.user.setPresence({"game": {"name": "Down | " + toTitleCase(latestStatus[serverID].population) + " Population"}, "status": "dnd"})
+      
+    }
+    
   }
 }
 
